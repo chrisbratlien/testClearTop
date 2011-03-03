8f1c66deae71fd2d75ad8ab0d8e021b058a96de5
Chris Bratlien <chrisbratlien@gmail.com>
2011-03-03 05:05:30 -0600

    document testservice.js

--- Resources/android/testservice.js
+++ Resources/android/testservice.js
@@ -4,7 +4,12 @@ var menuIntent = Ti.Android.createIntent({
     url: 'menu.js'
 });    
 
-console.log('flag',Titanium.Android.FLAG_ACTIVITY_CLEAR_TOP);
+console.log('flag value',Titanium.Android.FLAG_ACTIVITY_CLEAR_TOP);
+
+//Use case A: comment out the menuIntent.addFlags Each menu.js launched by the background activity every 15 seconds will stack up on the UI stack above the previous. If you let 4 or 5 cycles happen, you can Back button your way back through each of them.
+
+//Use case B: uncomment menuIntent.addFlags and rerun the app. This time, after 4 or 5 cycles, you can hit Back, but you'll not have a UI history of each prior activity.
 
 menuIntent.addFlags(Titanium.Android.FLAG_ACTIVITY_CLEAR_TOP);
-Ti.Android.currentActivity.startActivity(menuIntent);                
+
+Ti.Android.currentActivity.startActivity(menuIntent);
